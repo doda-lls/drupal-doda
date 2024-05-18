be66cbfe1b31589ccabbfaca59fc714f20d8decc
doda-lls <Danielle.Oda@lls.edu>
2024-05-17 17:03:29 -0700

    Slider works; css and js loaded. Tailwind does not work: error on -i input.css does not exist. Been working off the slider.css file. No custom fonts loaded.

---
+++ web/themes/custom/doda/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './{templates,components}/**/*.{twig,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
